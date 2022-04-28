1be8f3b7490e492886f8e95411d98c9e82acce55
rceccatelli <rebecca.ceccatelli@stud.unifi.it>
2022-04-28 13:00:40 +0200

    First commit: classes BSTNode and BinarySearchTree.

---
+++ BSTNode.py
@@ -0,0 +1,37 @@
+
+
+class BSTNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.father = None
+
+    def getKey(self):
+        return self.key
+
+    def setKey(self, key):
+        self.key = key
+
+    def getChildren(self):
+        children = []
+        if self.left is not None:
+            children.append(self.left)
+        if self.right is not None:
+            children.append(self.right)
+        return children
+
+    def setLeftChild(self, leftChild):
+        self.left = leftChild
+
+    def setRightChild(self, rightChild):
+        self.right = rightChild
+
+    def getFather(self):
+        return self.father
+
+    def setFather(self, father):
+        self.father = father
+
+    def print(self):
+        print(self.key)
---
+++ BinarySearchTree.py
@@ -0,0 +1,58 @@
+from BSTNode import BSTNode
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self,newNode):
+        if self.root is None:
+            self.setRoot(newNode)
+        else:
+            self.insertNode(self.root, newNode)
+
+    def setRoot(self, newNode):
+        self.root = newNode
+
+    def insertNode(self, currentNode, newNode):
+        if newNode.key <= currentNode.key:
+            if currentNode.left:
+                self.insertNode(currentNode.left, newNode)
+            else:
+                currentNode.left = newNode
+                newNode.father = currentNode
+        elif newNode.key > currentNode.key:
+            if currentNode.right:
+                self.insertNode(currentNode.right, newNode)
+            else:
+                currentNode.right = newNode
+                newNode.father = currentNode
+
+    def recursiveSearch(self, key):
+        return self.findNode(self.root, key)
+
+    def findNode(self, currentNode, key):
+        if currentNode is None:
+            return False, None
+        elif key == currentNode.key:
+            return True, currentNode
+        elif key < currentNode.key:
+            return self.findNode(currentNode.left, key)
+        else:
+            return self.findNode(currentNode.right, key)
+
+    def inOrderWalk(self):
+        self._inOrder(self.root)
+
+    def _inOrder(self,v):
+        if v is None:
+            return
+        if v.left is not None:
+            self._inOrder(v.left)
+        print(v.key)
+        if v.right is not None:
+            self._inOrder(v.right)
+
+    def getMaximum(self, currentNode):
+        while currentNode.right is not None:
+            currentNode = currentNode.right
+        return currentNode
