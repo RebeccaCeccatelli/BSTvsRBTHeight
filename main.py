935fd958552bc4b67e813792e9d4ad5fbf8f97d6
rceccatelli <rebecca.ceccatelli@stud.unifi.it>
2022-04-28 17:32:34 +0200

    Main: tutte le funzioni disponibili per il nodo funzionano correttamente.

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from RedBlackTree import RedBlackTree
 def main():
     rbt = RedBlackTree()
     A = [27,1,6,-54,22,34,30,132,0]
-    for x in A:
+    B = [13,15,7,6,2,20,18,1,9,52]
+    C = [10,9,8,7,6,5,4,3,2,1]
+    for x in C:
         rbt.rbInsert(RBTNode(x))
         rbt.inOrderWalk()
         print("--------")
+    found, node = rbt.recursiveSearch(9)
+    children = node.getChildren()
+    children[0].print()
+    children[1].print()
+    node.getFather().print()
+
 if __name__ == '__main__':
     main()
